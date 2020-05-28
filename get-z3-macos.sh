ac70ea6185e889eb6dfae257b37014a4041d0709
Kevin Laeufer <ekiwi@berkeley.edu>
2020-05-28 14:49:03 -0700

    copy to all the libs

--- get-z3-macos.sh
+++ get-z3-macos.sh
@@ -12,6 +12,8 @@ mv z3-$VERSION-x64-osx-10.14.6/ z3/
 # due to SIP we need to copy these libraries
 sudo cp z3/bin/libz3java.dylib  /Library/Java/Extensions
 cp z3/bin/libz3.dylib ~/lib
+sudo cp z3/bin/libz3.dylib /usr/local/lib
+sudo cp z3/bin/libz3.dylib /usr/lib
 
 # test
 #cp z3/bin/libz3.dylib .
