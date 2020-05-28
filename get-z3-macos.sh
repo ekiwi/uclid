138f5e9965c00791f55b23b0a5cfcd948d3ce339
Kevin Laeufer <ekiwi@berkeley.edu>
2020-05-28 14:01:21 -0700

    z3: do not copy to pwd

--- get-z3-macos.sh
+++ get-z3-macos.sh
@@ -12,8 +12,9 @@ mv z3-$VERSION-x64-osx-10.14.6/ z3/
 # due to SIP we need to copy these libraries
 sudo cp z3/bin/libz3java.dylib  /Library/Java/Extensions
 sudo cp z3/bin/libz3.dylib /usr/local/lib
-cp z3/bin/libz3java.dylib .
-cp z3/bin/libz3.dylib .
 
 # add z3 to path
 export PATH=$PATH:$(pwd)/z3/bin
+
+# copy z3 jar for sbt to find
+cp z3/bin/com.microsoft.z3.jar lib/
